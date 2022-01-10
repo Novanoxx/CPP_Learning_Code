67ddfc025aea53d7e6566bd2848d890e93fafd5f
Novanox <NovanoxCorp@outlook.fr>
2022-01-10 17:19:13 +0100

    test pour le qcm

---
+++ chap-01/test_qcm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    using namespace std;
+
+    // bool         a;
+    string       b;
+    vector<char> c;
+    auto         d = 4;
+
+    // cout << a << endl;
+    cout << "b: " << b << endl;
+    cout << "d: " << d << endl;
+    return 0;
+}
