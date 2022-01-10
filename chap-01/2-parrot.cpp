2c5b993499b2ec23954a8b800dfa8eb7ccd4d176
Novanox <NovanoxCorp@outlook.fr>
2022-01-10 16:31:58 +0100

    chap 1

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,29 @@
-// Implement the program here.
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    // int number = 0;
+    string word;
+    /*
+    while (true) {
+        std::cin >> number;
+        std::cout << "number : " << number << std::endl;
+        if (number == -1) {
+            break;
+        }
+    }
+
+   do {
+        std::cin >> number;
+        std::cout << "number : " << number << std::endl;
+   } while (number != -1);
+   */
+    for (int i = 0; i < 10; i++) {
+        // std::cin >> number;
+        cin >> word;
+        cout << "Quack ! " << word << " ! Quack ! " << endl;
+    }
+
+    return 0;
+}
--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,12 +1,36 @@
+// #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    int array[] = { 0, 1, 2, 3 };
+int main(int argc, char** argv) {
+    using namespace std;
 
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << array[i] << std::endl;
+    // int array[] = { 0, 1, 2, 3 };
+    if (argc < 2) {
+        cerr << "Expected argument for array size" << endl;
+        return -1;
+    }
+    int size = stoi(argv[2]);
+    // int size = atoi(argv[2])
+    if (size <= 0) {
+        cerr << "Expected strictly positive value for array size" << endl;
+    }
+    vector<int> array;
+    // cout << size << endl;
+    /*
+    int array[50] = {};
+
+    for (int i = 0; i < 50; i++) {
+        array[i] = i+1;
+    }
+
+    */
+    for (int i = 0; i < size; ++i) {
+        array.emplace_back(i + 1);
+    }
+    for (int value : array) {
+        cout << value << endl;
     }
 
     return 0;
