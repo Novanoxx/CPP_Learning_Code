e8b79368d02fdac1c9731bf5fd3f127f141b8db5
Novanox <NovanoxCorp@outlook.fr>
2022-01-16 23:32:11 +0100

    Complete c2-2

--- chap-02/1-first_class.cpp
+++ chap-02/1-first_class.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
-int main()
-{
-    // Person p;
+class Person {
+private:
+    std::string _name;
+    int         _age = 0;
 
-    // p.set_name("Batman");
-    // p.set_age(23);
+public:
+    void        set_name(std ::string name) { _name = name; }
+    void        set_age(int age) { _age = age; }
+    std::string get_name() const { return _name; }
+    int         get_age() const { return _age; }
+};
 
-    // std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+int main() {
+    Person p;
+
+    p.set_name("Batman");
+    p.set_age(23);
+
+    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
